d869a94f2151fe3d2752e22ccd5b766cc896d2e2
Santiago Brahim <135282900+SantiagoBrahim@users.noreply.github.com>
2025-06-19 16:52:47 -0300

    Feat(Added img to search filter)

--- src/Components/Table/SearchFilter.jsx
+++ src/Components/Table/SearchFilter.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
+import lupaImg from "../../assets/imgs/lupa.png"
 
 function SearchFilter({filterFunction}) {
     return (
-    <form className='d-flex justify-content-end align-items-center m-3 me-5' role='search'>
-        <div className="input-group input-group d-flex align-items-center w-25 gap-3">
-            <input type="search" placeholder='Buscar por Nombre o ID' className="form-control p-2 rounded-3 border border-primary"
-            onChange={
-                ((e) => {
-                    filterFunction(e.target.value);
-                })
-            }
-            />
-            <img src="#" alt="Buscar" />
-  </div>
-    </form>
+        <form className='d-flex justify-content-end align-items-center m-3 me-5' role='search'>
+            <div className="input-group input-group d-flex align-items-center w-25 gap-3">
+                <input type="search" placeholder='Buscar por Nombre o ID' className="form-control p-2 rounded-3 border border-primary"
+                onChange={
+                    ((e) => {
+                        filterFunction(e.target.value);
+                    })
+                }
+                />
+                <img src={lupaImg} alt="Buscar" width="30" height="30" />
+            </div>
+        </form>
     
   )
 }
