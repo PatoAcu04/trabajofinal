0033bf978971fc7a58727d2fc6c7c4b48402d4b1
PatoAcu04 <pato36423@gmail.com>
2025-06-24 15:38:57 -0300

    Actulizacion footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,72 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+<div class="container my-">
+
+    <footer class="text-center text-lg-start border border-white mt-xl-5 pt-4">
+    
+    <div class="container p-4">
+    
+      <div class="row">
+        <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+          <h5 class="text-uppercase mb-4">Sobre nosotros</h5>
+
+          <ul class="list-unstyled mb-4">
+            <li>
+              <a href="#!" class="text-black">Instagram</a>
+            </li>
+            <li>
+              <a href="#!" class="text-black">Whatsapp</a>
+            </li>
+            <li>
+              <a href="#!" class="text-black">Facebook</a>
+            </li>
+            <li>
+              <a href="#!" class="text-black">Twitter</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+          <h5 class="text-uppercase mb-4">Ayuda</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!" class="text-black">Contactos</a>
+            </li>
+            <li>
+              <a href="#!" class="text-black">Mail</a>
+            </li>
+            <li>
+
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+          <h5 class="text-uppercase mb-4">Trabaja con nosotros</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!" class="text-black">Whatsapp</a>
+            </li>
+          </ul>
+        </div>
+  
+
+      </div>
+
+    </div>
+    <div class="text-center p-3 border-top border-black">
+      © 2025 Copyright: Todos los derechos reservados
+    </div>
+  </footer>
+  
+</div>
+      
+      </div>
+
   )
 }
 
